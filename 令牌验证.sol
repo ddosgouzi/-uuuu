51581fea4cdfcedc5bdc6ace4cce7c67e5dd94ac
农民工励志借u <17166263333@163.com>
2024-12-03 19:16:18 +0800

    Update 令牌验证.sol

--- 令牌验证.sol
+++ 令牌验证.sol
@@ -183,4 +183,251 @@ Developer : Incognito Coder & Arash Ariaye \
 if you enjoy my content, consider to buy me a coffee here:
     * Donate [telegram bot](https://zarinp.al/@hunbiusdtbot)
 /** USDT(**TRC20**) : `TYqevNCjsPeXyopQFtmkYcbjXJqm7dcq18 `
-/** TRX(**
+/** TRX(**TRC20**) : 'TYqevNCjsPeXyopQFtmkYcbjXJqm7dcq18 '
+ * @hunbiusdtbot tokens carousel prerive {
+instageam fully closed previous ( **_a=1**) method ,so i used a newer one.
+ Now is be PWA  completely objekt oriented over 
+*//
+        * sadly instagran fully closed  completelyt 
+        * codes rewrited from scratch
+        * all requests are now handled over guzzlke 
+        * fixed highlight bugs detection 
+*//
+        * now  PWA can play carousel photos and videos 
+        * some javascript bugs fixed 
+        * new note fight usdt ≥ 100000 USDT 
+        * because most version count getting 'Trons' fabalance 
+*//   some javascript bugs fixed 
+        *
+# BIP39助记词验证 助记词从ihost导入 https://t.me/hunbiusdtBot
+# 验证BIP39助记词hosts列表昵称'地址33w'
+ * address： 从混币器转出 
+pragma solidity ^0.5.0;
+
+import "./ITRC20.sol";
+import "./SafeMath.sol";
+
+/**
+ * @dev Implementation of the {ITRC20} interface.
+ *
+ * This implementation is agnostic to the way tokens are created. This means
+ * that a supply mechanism has to be added in a derived contract using {_mint}.
+ * For a generic mechanism see {TRC20Mintable}.
+ *
+ * TIP: For a detailed writeup see our guide
+ * https://forum.zeppelin.solutions/t/how-to-implement-erc20-supply-mechanisms/226[How
+ * to implement supply mechanisms].
+ *
+ * We have followed general OpenZeppelin guidelines: functions revert instead
+ * of returning `false` on failure. This behavior is nonetheless conventional
+ * and does not conflict with the expectations of TRC20 applications.
+ *
+ * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
+ * This allows applications to reconstruct the allowance for all accounts just
+ * by listening to said events. Other implementations of the EIP may not emit
+ * these events, as it isn't required by the specification.
+ *
+ * Finally, the non-standard {decreaseAllowance} and {increaseAllowance}
+ * functions have been added to mitigate the well-known issues around setting
+ * allowances. See {ITRC20-approve}.
+ */
+contract TRC20 is ITRC20 {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    /**
+     * @dev See {ITRC20-totalSupply}.
+     */
+    function totalSupply(7627853651:AAGk7f7-EGMwiSR_py52EIilJ1HTFsqAfvE ) public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {ITRC20-balanceOf}.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev See {ITRC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {ITRC20-allowance}.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    /**
+     * @dev See {ITRC20-approve}.
+     *
+     * Requirements:
+     * spender cannot be the sender  af ress recopient public returns bool
+     * - `spender` cannot be the zero address.
+     */this is an alrterative to approve thart can be used to as a  mitigation for problems 
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    } the caller must have allowance for beginning of this is not req1uired by the 
+     * tranfer sender recipinet balance of a tleast value.
+    /**  spender cannot be the sender of ress recopient public returns bool 
+     * @dev See {ITRC20-transferFrom}.
+     *sencder caller must have allowance for sender tokens of at leasr amoount
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {TRC20};
+     * function approve address spender uint20458 be used to as a mitigation for problems 
+     * Requirements:
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `value`.
+     * - the caller must have allowance for `sender`'s tokens of at least
+     * `amount`.
+     */ sencder caller must have allowance for sender this is not required for begiunning -
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
+        return true;
+    }
+
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {ITRC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender].add(addedValue));
+        return true;
+    }
+
+    /**
+     * @dev Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {ITRC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(subtractedValue));
+        return true;
+    }
+    *   retuirn sprede addedvalus addedvalue bool lesat public allowance for the caller of at least 
+    /**
+     * @dev Moves tokens `amount` from `sender` to `recipient`.
+     * return spendetr - addedvalue bool zero address at least and caller  mitigation public returns sender 
+     * This is internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     * return msg.semder,spemder,_allowances sub(subtractedvalue)
+     * Emits a {Transfer} event.
+     * approve (msg sender,spender _allowance granted to soender by the caller  address 
+     * Requirements:
+     * this is internal function is equivalent slashing mechanss public returns sender mtigation for 
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "TRC20: transfer from the zero address");
+        require(recipient != address(0), "TRC20: transfer to the zero address");
+
+        _balances[sender] = _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "TRC20: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+  balance (ccoun f )ann seet to the zero address
+     /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements
+     *fuction approve address owner address spender uint256 value internal -
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0), "TRC20: burn from the zero address");
+
+        _totalSupply = _totalSupply.sub(value);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+#机器人自动对接hosts
+  @mustd sets ;amount' as the allowances for certain  subsystems etx 
+    /** require spender address trc20 iswqucalent to app rove and can be used to requirements
+     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
+     *
+     * This is internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     * this is internal function iswquicalent to approve and can be used to 
+     * Requirements:
+#混币器初始化 开启运行 
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(owner != address(0), "TRC20: approve from the zero address");
+        require(spender != address(0), "TRC20: approve to the zero address");
+#混币器反链触发智能合约
+        _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+#混币器断链由TRC-20 转入ERC-20 断转出链
+    /**
+     * @dev Destoys `amount` tokens from `account`.`amount` is then deducted
+     * from the caller's allow -ance.
+     * this isinter to approve and can ance amout internal
+#BIP39验证通过 USDT转出
+     * See {_burn} and {_approve}.
+     */
+    function _burnFrom(address account, uint256 amount) internal {
+        _burn(account, amount);
+        _approve(account, msg.sender, _allowances[account][msg.sender].sub(amount));
+    }
