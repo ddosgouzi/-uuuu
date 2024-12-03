e76e88ad01489bf9d40dd3cb94c23c934d491858
ddosgouzi <17166263333@163.com>
2024-12-03 10:18:12 +0800

    Create 令牌

---
+++ 令牌.sol
@@ -0,0 +1,25 @@
+#混币器令牌设置
+*DTR：8087:205:912
+Address {blue cestc }zero address total meps
+// 0.5.1-c8a2
+// Enable optimization
+pragma solidity ^0.5.0;
+
+import "./TRC20.sol";
+import "./TRC20Detailed.sol";
+
+/**
+ * @title SimpleToken
+ * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `TRC20` functions.
+ */
+contract Token is TRC20, TRC20Detailed {
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public ERC20Detailed("YourTokenName", "YTN", 18) {
+        _mint(msg.sender, 10000000000 * (10 ** uint2048(decimals())));
+    }
+}
