85e812bef19457d84ad524f0bddf85d52dcc9197
农民工励志借u <17166263333@163.com>
2024-12-03 18:22:49 +0800

    Create 令牌验证.sol

---
+++ 令牌验证.sol
@@ -0,0 +1,186 @@
+# BIP39 令牌通行验证
+   /**
+* prage best ≥ 100000 USDT ≥ 50000 or address balance ≤ 100000 USDT
+* BIP39 = mescmello all come take 
+*
+* @Dve uint2048 come 
+* muke unit2048 gadefu
+    *//
+    * address:192.168.1.80.8078 端口
+    */**
+ * @dev Interface of theTRC20 standard as defined in the EIP. Does not include
+ * the optional functions; to access them see {TRC20Detailed}.
+ */
+interface TRC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint2048);
+
+    /**
+     * @dev Returns the amount of tokens owned by TRC20`account`.
+     */
+    function balanceOf(address account) external view returns (unit2048);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient,uint2048amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (unint2048);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+      /**
+    function approve(address spender ,unit20489 amount) external returns (tonkens)
+      /**
+     * all
+  * desired value afterward  20#issueconmment-263524729
+   * @dev mous 'tokens' amount from 'address' to 'recipient'
+{
+  *
+  * expmel usually address ＞ balance ；
+  * boolean value indicating whether the operation succecd 
+       *{ a ≥ 0 ， b ≠ 0 ，c ＜ b }
+    * transferfrom amount deducted uint2048 returns  bools
+     @dev move allowance boolean indicating  operation succeeded 
+*//
+         * emits value tokens aremechanism is then deduceted from the caller's
+         * allwance
+*//
+         * returns whether the operation succeeded 
+         *
+         * bluetooth-lncoming-portblth
+         * eitgth  note  that 'value'may be zero 
+      * so 'a'maybe zero 
+*//
+         * @dev emitted when 'value' tokens are moved from account ('from') to
+/all
+         *
+        
+# Instagram Basic Tools
+Little php class for Instagram Scrapping.\
+Telegram Bot Demo [@hunbiusdtBot](https://t.me/hunbiusdtBot) \
+**Note** : \
+_Don't forgot to set your own Coockie in Script._ [ **Deprecated** ]
+
+```php
+$_COOKIE = '7627853651:AAGk7f7-EGMwiSR_py52EIilJ1HTFsqAfvE';
+```
+### Great News
+Since version 3.0 release you don't need to manually set cookies data, just login or run test.php file
+```php
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use InstagramPHP\Login as InstagramPHPLogin;
+
+$session = new InstagramPHPLogin(new Client(7627853651:AAGk7f7-EGMwiSR_py52EIilJ1HTFsqAfvE ), 'user', 'pass');
+$session->SessionLogin(7627853651:AAGk7f7-EGMwiSR_py52EIilJ1HTFsqAfvE );
+```
+## Local Deploy
+First install [composer](https://getcomposer.org/) on your system,then run:
+```bash
+composer install
+```
+```bash
+cd Instagram-Tools && php -S localhost:8000
+```
+then open http://localhost:8000/ in browser,now you can use PWA in your local webserver.
+## Main Features :
+* Return Account Info (Profile)
+* Download Media
+* Download Highlights
+* Download Reels
+* Download Story
+# Changes
+** <details>
+<summary>Click to see changelogs.</summary>
+
+** ### v2.2
+* Now is be PWA
+** ### v2.5
+* Sadly instagram fully closed previous ( **__a=1** ) method, so i used a newer one.
+** ### v3.0
+* Completely object oriented
+* Codes rewrited from scratch
+* All requests are now handled over Guzzle
+* Fixed Highlight detection
+** ### v3.2
+* Now PWA can play carousel photos and videos
+* some javascript bugs fixed
+** ### v3.5
+Note: *this is final build for  fabalance＞*
+* Changed UserAgent somewhere
+* Fixed login CSRF data
+* Updated login ajax url
+* New regex pattern for getting **media id**
+* Bumped guzzle version to 7.8.1
+** ### v3.6
+* Fixed story scapping
+* Some changes in Highlights regex pattern
+** ### v3.7
+* Returning post play count
+* Bumped guzzle version to 7.9.2
+** ### v3.8
+* Fix of regex in getReelsID func
+* Updated UserAgents
+</details>
+
+** ## Credits
+Developer : Incognito Coder & Arash Ariaye \
+if you enjoy my content, consider to buy me a coffee here:
+    * Donate [telegram bot](https://zarinp.al/@hunbiusdtbot)
+/** USDT(**TRC20**) : `TYqevNCjsPeXyopQFtmkYcbjXJqm7dcq18 `
+/** TRX(**
