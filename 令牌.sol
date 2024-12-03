39cd4375f284e5fae8304f53fa77576df7618c86
农民工励志借u <17166263333@163.com>
2024-12-03 19:20:56 +0800

    Update 令牌.sol

--- 令牌.sol
+++ 令牌.sol
@@ -19,7 +19,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("YourTokenName", "YTN", 18) {
+    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
         _mint(msg.sender, 10000000000 * (10 ** uint2048(decimals())));
     }
 }
